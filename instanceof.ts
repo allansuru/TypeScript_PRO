0e2f98cd56462e96f24cc94585b79ac8e5299642
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-11-25 13:04:33 -0200

    Literal Type Guards and 'in' Operator

--- instanceof.ts
+++ instanceof.ts
@@ -7,11 +7,14 @@ const bar = new Foo();
 // console.log(bar instanceof Foo);
 // console.log(Object.getPrototypeOf(bar) === Foo.prototype);
 
+
 class Song2 {
+    kind: 'song2';
     constructor(public title: string, duration: number) {}
 }
 
 class Playlist {
+    kind: 'playlist';
     constructor(public name: string, public songs: Song2[]){}
 }
 
@@ -22,15 +25,20 @@ function getItemName(item: Song2 | Playlist) {
         return item.songs;
 }
 
-function isSong(item: any) {
-    return item instanceof Song2
+function isSong(item: any): item is Song2 {
+    //return item instanceof Song2
+    return 'title' in item;
 }
 
+
 function getItemNameOther(item: Song2 | Playlist) {
-     if (isSong(item)) {
-         return (item as Song2).title;
-     }
-     return (item as Playlist).name;
+    //  if (isSong(item)) {
+    //      return item.title;
+    //  
+    if (item.kind === 'song2') {
+        return item.title;
+    }
+     return item.name;
 }
 
 const songName = getItemName(new Song2('GodPlan', 30000));
@@ -40,4 +48,4 @@ const playListNames = getItemName(
     new Playlist('The Best Songs', [new Song2('GodPlans', 300000), new Song2('Passageiro', 30000)])
 )
 
-console.log('PlayList: ', playListNames);
+// console.log('PlayList: ', playListNames);
