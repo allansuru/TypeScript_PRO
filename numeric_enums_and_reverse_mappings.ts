5b833f3ba1fe2b981eaac3557b0fdf076f076c2c
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-11-27 21:22:14 -0200

    Numeric Enums and Reverse Mappings

---
+++ numeric_enums_and_reverse_mappings.ts
@@ -0,0 +1,19 @@
+enum Sizes {
+    Small,
+    Medium,
+    Large,
+}
+
+enum Sizes {
+    ExtraLarge = 3
+}
+
+console.log(Sizes.Large, Sizes[Sizes.Large]);
+
+
+console.log(Sizes);
+
+const selectedSize = 3
+
+console.log(Sizes[selectedSize]);
+
