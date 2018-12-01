e9ec40bf41b0ab4705500fa04abdcc6b5c07e157
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-12-01 12:33:39 -0200

    Augmenting Modules with Declarations

--- src/@types/lodash/index.d.ts
+++ src/@types/lodash/index.d.ts
@@ -1,3 +1,9 @@
+import * as lodash from 'lodash';
+
 declare module 'lodash' {
-    export function chunk<T>(collection: T, size?: number): any[][]
+    //export function chunk<T>(collection: T, size?: number): any[][]
+
+    interface LoDashStatic {
+        log<T>(item: T): void;
+    }
 }
