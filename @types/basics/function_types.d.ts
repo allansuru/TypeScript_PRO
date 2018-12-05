c020d306c2b62f07083d6d929df4703919c49e4e
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-12-05 20:38:02 -0200

    Functions exemples

--- @types/basics/function_types.d.ts
+++ @types/basics/function_types.d.ts
@@ -1,2 +1,5 @@
-declare let sumOrOrder: Function;
-declare const soma: any;
+declare let sumOrOrder: (price: number, quantity?: number) => number;
+declare const soma: number;
+declare const soma2: number;
+declare const soma3: number;
+declare const soma4: number;
--- src/basics/function_types.ts
+++ src/basics/function_types.ts
@@ -6,14 +6,33 @@
 
 // OR
 
-let sumOrOrder: (price: number, quantity: number) => number;
+// Functions and Optional Arguments
+
+
+let sumOrOrder: (price: number, quantity?: number) => number;
 
 
 sumOrOrder = (price, quantity): number => {
-    return price * quantity;
+    if (quantity) {
+        return price * quantity;
+    } 
+    return price;
 }
 
 
 const soma = sumOrOrder(20,2);
+const soma2 = sumOrOrder(20);
 
 console.log(soma);
+console.log(soma2);
+
+// Typed Functions and Default Params
+
+sumOrOrder = (price, quantity = 5): number => {
+    return price * quantity;
+}
+
+const soma3 = sumOrOrder(20);
+const soma4 = sumOrOrder(100, 5);
+console.log(soma3);
+console.log(soma4);
