daa14565bffdb08e023978346b5a6cd43d6f7cfa
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-12-06 20:48:28 -0200

    Arrays exemples

---
+++ src/basics/arrayTypes_and_generics.ts
@@ -0,0 +1,16 @@
+// Array Types and Generics
+
+let sizes: string[] = ['small', 'medium', 'large'];
+
+// OR
+
+let tops: Array<string>;
+
+tops = ['A', 'B', 'C'];
+
+
+// Tuple Types for Arrays
+
+let tuple: [string, number, boolean];
+
+tuple = ['teste', 1, true];
