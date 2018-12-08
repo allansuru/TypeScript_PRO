6999559cbaaf4feef4e0139cf5cace7e45d187c6
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2018-12-08 15:02:50 -0200

    Type Assertions

--- @types/basics/type_aliases.d.ts
+++ @types/basics/type_aliases.d.ts
@@ -1,3 +1,11 @@
 declare type Size = 'small' | 'medium' | 'large';
 declare let pizzaSize: Size;
 declare const selectSizeA: (size: "small" | "medium" | "large") => void;
+declare type Pizza123 = {
+    name: string;
+    toppings: number;
+};
+declare const pizza123: Pizza123;
+declare const serialized: string;
+declare function getNameFromJSON(obj: string): any;
+declare const voltaJSON: any;
--- src/basics/type_aliases.ts
+++ src/basics/type_aliases.ts
@@ -7,3 +7,26 @@ const selectSizeA = (size: Size) => {
 }
 
 selectSizeA('small');
+
+
+// Type Assertions
+
+type Pizza123 = { name: string, toppings: number };
+
+
+const pizza123: Pizza123 = { name: 'Bacon', toppings: 123 };
+
+console.log('Antes de serializar: ', pizza123);
+
+const serialized = JSON.stringify(pizza123);
+
+console.log('Depois de serializar: ', serialized);  
+
+function getNameFromJSON(obj: string): any {
+    return (<Pizza123>JSON.parse(obj));
+    // return (<Pizza123>JSON.parse(obj)).name;
+}
+
+const voltaJSON = getNameFromJSON(serialized);
+
+console.log('Transformando em JSON again: ', voltaJSON);
